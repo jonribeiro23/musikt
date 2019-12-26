919db9bc1e09d97cdad462beb43afd0ca696e060
jonribeiro23 <jonathas_tc@hotmail.com>
2019-12-25 22:24:42 -0300

    aulas

--- static/js/media.js
+++ static/js/media.js
@@ -4,6 +4,7 @@ function myFunction(x) {
     let col1 = document.querySelectorAll('#col1')
     let col2 = document.querySelectorAll('#col2')
     let mg = document.querySelectorAll('#mg')
+    let curso = document.querySelectorAll('#curso')
 
     col1.forEach((x) => {
       x.classList.remove("col-8")
@@ -17,16 +18,27 @@ function myFunction(x) {
       x.style.margin = '0 10px 0 10px'
     })
 
+    curso.forEach((x) => {
+      x.classList.remove("col-4")
+      x.setAttribute('class', 'col-12')
+    })
+
   } else {
 
     let col1 = document.querySelectorAll('#col1')
     let col2 = document.querySelectorAll('#col2')
+    let curso = document.querySelectorAll('#curso')
 
     col1.forEach((x) => {
       x.setAttribute('class', 'col-8')
     })
 
     col2.forEach((x) => {
+      x.setAttribute('class', 'col-4')
+    })
+
+    curso.forEach((x) => {
+      x.classList.remove("col-12")
       x.setAttribute('class', 'col-4')
     })
 
